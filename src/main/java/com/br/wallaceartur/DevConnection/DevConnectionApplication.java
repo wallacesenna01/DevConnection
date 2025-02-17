b95f954099ac08df67660265bd6871c4bcfca0f4
Wallace Artur <wallacearthur_3@hotmail.com>
2025-02-17 12:36:06 -0300

    Project Created

---
+++ src/main/java/com/br/wallaceartur/DevConnection/DevConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.br.wallaceartur.DevConnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevConnectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/wallaceartur/DevConnection/DevConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.wallaceartur.DevConnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
