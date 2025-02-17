c25cccd7fec9e81340c6fff17c640f1373ec52e5
Wallace Artur <wallacearthur_3@hotmail.com>
2025-02-17 14:50:20 -0300

    TokenService

---
+++ src/main/java/com/br/wallaceartur/DevConnection/security/TokenService.java
@@ -0,0 +1,50 @@
+package com.br.wallaceartur.DevConnection.security;
+
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    private final String secret = "segredo";
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String token = JWT.create()
+                    .withIssuer("auth")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(getExpirationDate())
+                    .sign(algorithm);
+            return token;
+        } catch (Exception e) {
+            throw new RuntimeException("ERROR WHILE GENERATING TOKEN", e);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            DecodedJWT decodeJWT = JWT.require(algorithm)
+                    .withIssuer("auth")
+                    .build()
+                    .verify(token);
+            return decodeJWT.getSubject();
+
+        } catch (Exception exception) {
+            return "";
+        }
+
+    }
+
+    public Date getExpirationDate() {
+        return new Date(System.currentTimeMillis() + 2 * 60 * 60 * 1000);
+    }
+}
